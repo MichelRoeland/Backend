03109a1cb8ed1cf6d4fad5e011c6f8346d417758
Auri <auriorx@gmail.com>
2018-04-07 20:39:55 +0200

    SaveData button implemented with sample data

--- PatientApp/PatientApp/PatientApp/Views/InsightRequests.xaml.cs
+++ PatientApp/PatientApp/PatientApp/Views/InsightRequests.xaml.cs
@@ -1,5 +1,4 @@
-﻿using PatientApp.Controls;
-using PatientApp.Models;
+﻿using PatientApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,11 +19,11 @@ namespace PatientApp.Views
             // Create stub data
             InsightRequest[] stubList = {
                     new InsightRequest { doctorName = "Doctor Hank", requestDate = DateTime.Now, isApproved = false, isProcessed = false, doctorAddress = "doctorkey001", patientAddress = "patientkey001" },
-                    new InsightRequest { doctorName = "Doctor Frank", requestDate = DateTime.Now, isApproved = false, isProcessed = true, doctorAddress = "doctorkey001", patientAddress = "patientkey001" },
-                    new InsightRequest { doctorName = "Doctor Tank", requestDate = DateTime.Now, isApproved = true, isProcessed = true, doctorAddress = "doctorkey001", patientAddress = "patientkey001" },
-                    new InsightRequest { doctorName = "Doctor Skank", requestDate = DateTime.Now, isApproved = true, isProcessed = true, doctorAddress = "doctorkey001", patientAddress = "patientkey001" },
-                    new InsightRequest { doctorName = "Doctor Bank", requestDate = DateTime.Now, isApproved = false, isProcessed = false, doctorAddress = "doctorkey001", patientAddress = "patientkey001" },
-                    new InsightRequest { doctorName = "Doctor Prank", requestDate = DateTime.Now, isApproved = false, isProcessed = true, doctorAddress = "doctorkey001", patientAddress = "patientkey001" }
+                    new InsightRequest { doctorName = "Doctor Frank", requestDate = DateTime.Now, isApproved = false, isProcessed = true, doctorAddress = "doctorkey002", patientAddress = "patientkey001" },
+                    new InsightRequest { doctorName = "Doctor Tank", requestDate = DateTime.Now, isApproved = true, isProcessed = true, doctorAddress = "doctorkey003", patientAddress = "patientkey001" },
+                    new InsightRequest { doctorName = "Doctor Skank", requestDate = DateTime.Now, isApproved = true, isProcessed = true, doctorAddress = "doctorkey004", patientAddress = "patientkey001" },
+                    new InsightRequest { doctorName = "Doctor Bank", requestDate = DateTime.Now, isApproved = false, isProcessed = false, doctorAddress = "doctorkey005", patientAddress = "patientkey001" },
+                    new InsightRequest { doctorName = "Doctor Prank", requestDate = DateTime.Now, isApproved = false, isProcessed = true, doctorAddress = "doctorkey006", patientAddress = "patientkey001" }
                 };
 
             List<InsightRequest> toApprove = new List<InsightRequest>();
@@ -58,13 +57,15 @@ namespace PatientApp.Views
 
         public void FillData(Xamarin.Forms.Grid x, List<InsightRequest> toApprove)
         {
+            int fz = 12;
             x.RowDefinitions = new RowDefinitionCollection
             {
-                new RowDefinition { Height = GridLength.Auto }
+                new RowDefinition { Height = 40},
+                new RowDefinition { Height = 40}
             };
-
             x.ColumnDefinitions = new ColumnDefinitionCollection
             {
+                new ColumnDefinition { Width = GridLength.Star },
                 new ColumnDefinition { Width = GridLength.Star },
                 new ColumnDefinition { Width = GridLength.Star },
                 new ColumnDefinition { Width = GridLength.Star },
@@ -74,25 +75,50 @@ namespace PatientApp.Views
             int r = 0;
             foreach (InsightRequest i in toApprove)
             {
-                x.Children.Add(new Label { Text = i.doctorName }, 0, r);
-                x.Children.Add(new Label { Text = i.requestDate.ToString() }, 1, r);
+                // Create list of possible items to pic
+                Dictionary<string, Boolean> list = new Dictionary<string, Boolean>
+                {
+                    { "Accept", true },
+                    { "Deny", false }
+                };
+
+                Picker picker = new Picker { Scale = 0.60, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.Center, WidthRequest = 70 };
+
+                foreach (string pk in list.Keys)
+                {
+                    picker.Items.Add(pk);
+                }
+
+                x.Children.Add(new Label { Text = i.doctorName, FontSize = fz, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand}, 0, r);
+                x.Children.Add(new Label { Text = i.requestDate.ToString(), FontSize = fz, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand }, 1, r);
                 if (x == this.PendingData)
                 {
-                    x.Children.Add(new Label { Text = "-" }, 2, r);
+                    x.Children.Add(new Label { Text = "-", FontSize = fz, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand }, 2, r);
                 }
                 else if (x == this.ApprovedData)
                 {
-                    x.Children.Add(new Label { Text = "Yes" }, 2, r);
+                    x.Children.Add(new Label { Text = "Yes", FontSize = fz, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand }, 2, r);
+                    picker.SelectedIndex = 0;
                 }
                 else
                 {
-                    x.Children.Add(new Label { Text = "No" }, 2, r);
+                    x.Children.Add(new Label { Text = "No", FontSize = fz, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand }, 2, r);
+                    picker.SelectedIndex = 1;
                 }
-                x.Children.Add(new Label { Text = "-" }, 3, r);
+                x.Children.Add(picker, 3, r);
+                x.Children.Add(new Label { Text = i.doctorAddress, FontSize = fz, VerticalOptions = LayoutOptions.CenterAndExpand, HorizontalOptions = LayoutOptions.CenterAndExpand }, 4, r);
 
                 r++;
             }
         }
 
-	}
+        public void SaveData(object sender, EventArgs args)
+        {
+            foreach(View x in PendingData.Children)
+            {
+                PendingData.Children.Add( new Label { Text = x.ToString()});
+            }
+        }
+
+    }
 }
